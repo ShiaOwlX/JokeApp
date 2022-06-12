097af57848540d43bbb1c9b93cd706a9ad2d5022
Shia Nussbaum <shia@owlx.com>
2022-06-12 17:34:31 -0400

    Function to prep string from and to CSV

---
+++ JokeApp/Database/TextHandling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokeApp
+{
+    public static class TextHandling
+    {
+        /// <summary>
+        /// Wrap text in qoutes and swap '"' for '""'
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string PrepStringForCSV(this string value)
+        {
+            return "\"" + value.Replace(oldValue: "\"", newValue: "\"\"") + "\"";
+        }
+        /// <summary>
+        /// Remove CSV wrappings
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string PrepStringFromCSV(this string value)
+        {
+            return value.Substring(1, value.Length - 2).Replace(oldValue: "\"\"", newValue: "\"");
+        }
+    }
+}
