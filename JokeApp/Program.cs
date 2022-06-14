dbf398ab53c3a3e39b77bda68cba699582f46cd5
Shia Nussbaum <shia@owlx.com>
2022-06-14 16:50:18 -0400

    Updated JokeClient to "using"

--- JokeApp/Program.cs
+++ JokeApp/Program.cs
@@ -3,23 +3,25 @@ using JokeApp.Database;
 using System.Configuration;
 
 // set up
-var jokeClient = new JokeClient();
+
 string Filename = ConfigurationManager.AppSettings["filename"] ?? "lostJokes.csv";
 
 // load saved data
 List<JokeModel> Jokes = Filename.FilePath().ReadFile();
 
 // get a new joke and save it
-JokeModel Joke = await jokeClient.GetJokeAsync();
-Jokes.Add(Joke);
-Jokes.WriteFile(Filename);
+using (var jokeClient = new JokeClient())
+{
+    JokeModel Joke = await jokeClient.GetJokeAsync(JokeCategory.Any);
+    Jokes.Add(Joke);
+    Jokes.WriteFile(Filename);
 
-// display info to user
-Console.WriteLine(Joke.Setup);
-Console.WriteLine(Joke.Delivery);
+    // display info to user
+    Console.WriteLine(Joke.Setup);
+    Console.WriteLine(Joke.Delivery);
 
-Console.WriteLine("\n");
-Console.WriteLine($"Category: {Joke.Category}");
-Console.WriteLine($"Joke id: {Joke.ID}");
-
+    Console.WriteLine("\n");
+    Console.WriteLine($"Category: {Joke.Category}");
+    Console.WriteLine($"Joke id: {Joke.ID}");
+}
 //Console.ReadLine();
