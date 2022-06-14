693c1e021c42947b15a7496e4f615db940698867
Shia Nussbaum <shia@owlx.com>
2022-06-14 16:51:13 -0400

    Added dispose and Enum param

---
+++ JokeApp/JokeCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokeApp
+{
+    [Flags]
+    public enum JokeCategory
+    {
+        Any = 0,
+        Programming = 1,
+        Miscellaneous = 2,
+        Dark = 4,
+        Pun = 8,
+        Spooky = 16,
+        Christmas = 32
+    }
+
+}
